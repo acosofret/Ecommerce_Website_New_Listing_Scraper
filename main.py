f464fcfe1cf12623d8049d37794e267b734de794
acosofret <acosofretcwst@gmail.com>
2024-05-28 22:15:43 +0100

    The program scraps thru the searchs of a category and pulls up the data for products listings. However, the page has a dynamic scroll functionality meaning that more products are loading as you scroll down. The program is only capable to pull the data for the first few listings displayed by default. Further options to be researched.

---
+++ main.py
@@ -0,0 +1,24 @@
+
+from bs4 import BeautifulSoup
+import requests
+
+nike_mens_shoes = "https://www.nike.com/w/mens-shoes-nik1zy7ok"
+nike_womens_shoes = "https://www.nike.com/w/womens-shoes-5e1x6zy7ok"
+nike_kids_shoes = "https://www.nike.com/w/kids-shoes-v4dhzy7ok"
+
+response = requests.get(nike_mens_shoes)
+
+soup = BeautifulSoup(response.text, "html.parser")
+
+product_cards = soup.find_all("div", class_="product-card")
+product_info_list = []
+
+for card in product_cards:
+	link = card.find("a", class_="product-card__link-overlay")["href"]
+	title = card.find("div", class_="product-card__title").text.strip()
+	product_info_list.append({"title": title, "link":link})
+
+for product_info in product_info_list:
+	print(product_info)
+# products = soup.select("li ul li h3")
+# song_names = [song.getText().strip() for song in song_names_spans]
